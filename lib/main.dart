dce66632878e548675e16df16ab108b6aa348f44
mmartyndev <hello@mmartyn.dev>
2022-09-23 18:30:47 -0300

    Remove comments

--- lib/main.dart
+++ lib/main.dart
@@ -1,12 +1,3 @@
-// This was built in 10 minutes during the recording of
-// https://www.youtube.com/watch?v=G1l8U8DTkvk.
-//
-// It has all the problems and bugs. I didn't touch
-// a think in this file after recording the video.
-//
-// You might want to hide DartPad's warnings by clicking
-// "hide" in the bottom status bar.
-
 import 'package:flutter/material.dart';
 
 void main() {
@@ -16,21 +7,12 @@ void main() {
 class MyApp extends StatelessWidget {
   const MyApp({Key? key}) : super(key: key);
 
-  // This widget is the root of your application.
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
+      debugShowCheckedModeBanner: false,
       title: 'Flutter Demo',
       theme: ThemeData(
-        // This is the theme of your application.
-        //
-        // Try running your application with "flutter run". You'll see the
-        // application has a blue toolbar. Then, without quitting the app, try
-        // changing the primarySwatch below to Colors.green and then invoke
-        // "hot reload" (press "r" in the console where you ran "flutter run",
-        // or simply save your changes to "hot reload" in a Flutter IDE).
-        // Notice that the counter didn't reset back to zero; the application
-        // is not restarted.
         primarySwatch: Colors.blue,
       ),
       home: const MyHomePage(title: 'Flutter Demo Home Page'),
@@ -41,15 +23,6 @@ class MyApp extends StatelessWidget {
 class MyHomePage extends StatefulWidget {
   const MyHomePage({Key? key, required this.title}) : super(key: key);
 
-  // This widget is the home page of your application. It is stateful, meaning
-  // that it has a State object (defined below) that contains fields that affect
-  // how it looks.
-
-  // This class is the configuration for the state. It holds the values (in this
-  // case the title) provided by the parent (in this case the App widget) and
-  // used by the build method of the State. Fields in a Widget subclass are
-  // always marked "final".
-
   final String title;
 
   @override
@@ -57,22 +30,13 @@ class MyHomePage extends StatefulWidget {
 }
 
 class _MyHomePageState extends State<MyHomePage> {
-
-
   var tiles = List.filled(9, 0);
 
   @override
   Widget build(BuildContext context) {
-    // This method is rerun every time setState is called, for instance as done
-    // by the _incrementCounter method above.
-    //
-    // The Flutter framework has been optimized to make rerunning build methods
-    // fast, so that you can just rebuild anything that needs updating rather
-    // than having to individually change instances of widgets.
     return Scaffold(
       appBar: AppBar(
-        // Here we take the value from the MyHomePage object that was created by
-        // the App.build method, and use it to set our appbar title.
+        centerTitle: true,
         title: Text(widget.title),
       ),
       body: Row(
@@ -94,8 +58,8 @@ class _MyHomePageState extends State<MyHomePage> {
                         child: Text(tiles[i] == 0
                             ? ''
                             : tiles[i] == 1
-                            ? 'X'
-                            : 'O')),
+                                ? 'X'
+                                : 'O')),
                   )
               ],
             ),
@@ -106,8 +70,8 @@ class _MyHomePageState extends State<MyHomePage> {
               Text(isWinning(1, tiles)
                   ? 'You won!'
                   : isWinning(2, tiles)
-                  ? 'You lost!'
-                  : 'Your move'),
+                      ? 'You lost!'
+                      : 'Your move'),
               OutlinedButton(
                   onPressed: () {
                     setState(() {
